f2dc297f85afdcfb7384a73fee129661e2dd93b4
kuzak <kuzakovevgen@gmail.com>
2022-09-27 00:27:44 +0700

    1

--- laba10/Form1.Designer.cs
+++ laba10/Form1.Designer.cs
@@ -82,7 +82,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- laba10/Form1.cs
+++ laba10/Form1.cs
@@ -7,21 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Schema;
 
 namespace laba10
 {
     public partial class Form1 : Form
     {
+        Pen pen1 = new Pen(Brushes.Red, 2);
         const int border = 20;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         { 
@@ -29,14 +27,27 @@ namespace laba10
             BorderlessForm inside_form = new BorderlessForm();
             inside_form.TopLevel = false;
             inside_form.Location = new Point(border, border);
+
+            inside_form.Size = new Size(500,200);
+            Bitmap bm = new Bitmap(inside_form.Width,inside_form.Height);
+            Graphics g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            inside_form.BackgroundImage = bm;
+
             this.Controls.Add(inside_form);
             inside_form.Show();
         } 
 
+        private void Control1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+            }
+        }
 
         private void cicleButton1_Click_1(object sender, EventArgs e)
         {
- if (colorDialog1.ShowDialog() == DialogResult.OK)
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
             {
                 Color c = colorDialog1.Color;
             }
